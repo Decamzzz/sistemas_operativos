324e53f55cd18877c3be1799706dfcebf618bae1
Decamzzz <botinacamilo44@gmail.com>
2025-10-09 20:32:04 -0500

    first_testing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from GUI import Simulador
 from PySide6.QtWidgets import QApplication
 
 if __name__ == "__main__":
+    print("Simulacion en proceso...")
     app = QApplication(sys.argv)
     ventana = Simulador()
     ventana.show()
